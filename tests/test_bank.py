c925ef6da3c595a7b6396a835b68d93fe342e184
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-09-15 11:33:14 +0100

    Added add funds tests.

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -3,7 +3,7 @@
 import pytest
 
 from bank_api.bank import Bank, Account
-
+from datetime import datetime
 
 @pytest.fixture
 def bank() -> Bank:
@@ -45,4 +45,29 @@ def test_cannot_modify_accounts_set(bank):
 
 
 # TODO: Add unit tests for bank.add_funds()
+def test_add_funds_with_account(bank):
+    assert len(bank.transactions) == 0
+    account_name = "New Account"
+    bank.create_account(account_name)
+    bank.add_funds(account_name, 1)
+    assert len(bank.transactions) == 1
+    tx = list(bank.transactions)[0]
+    assert tx.account.name == account_name
+    assert tx.date.date() == datetime.now().date()
+    assert tx.amount == 1
 
+def test_add_funds_no_accounts(bank):
+    assert len(bank.transactions) == 0
+    with pytest.raises(ValueError):
+        bank.add_funds("New Account", 1)
+
+def test_add_funds_negative_amount(bank):
+    assert len(bank.transactions) == 0
+    account_name = "New Account"
+    bank.create_account(account_name)
+    bank.add_funds(account_name, -1)
+    assert len(bank.transactions) == 1
+    tx = list(bank.transactions)[0]
+    assert tx.account.name == account_name
+    assert tx.date.date() == datetime.now().date()
+    assert tx.amount == -1
