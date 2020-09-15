2be68526d8a78cde7519040bc9000a4411c06ba1
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-09-15 12:08:06 +0100

    Added test to add money.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 """Integration tests for app.py"""
 import pytest
-
+from bank_api.bank import Bank
 from bank_api.app import app
 
 
@@ -23,9 +23,19 @@ def test_account_creation(client):
     assert result.status_code == 200
     assert result.json == {'name': 'new_account'}
 
-    result = client.post('/accounts/new_account')
-    assert result.status_code == 200
-    assert result.json == {'name': 'new_account'}
 
+def test_get_unknown_account(client):
     result = client.get('/accounts/unknown_account')
     assert result.status_code == 404
+
+@pytest.fixture
+def bank() -> Bank:
+    return Bank()
+
+def test_add_money(client, bank):
+    bank.create_account('new_account')
+    result = client.post('/accounts/money', json={
+        "name": "new_account",
+        "amount": 1
+    })
+    assert result.status_code == 200
